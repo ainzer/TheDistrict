a0a594588c472857988debe690392bd6193db82a
Tony <127862337+ainzer@users.noreply.github.com>
2024-03-26 16:02:44 +0100

    26/03/2024

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -1,18 +1,57 @@
 <?php
-
 namespace App\Controller;
 
+use App\Entity\Plat;
+use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/panier', name: 'panier_')]
 class PanierController extends AbstractController
 {
-    #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, PlatRepository $platRepository)
     {
-        return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+        $panier = $session->get('panier', []);
+
+        // On initialise des variables
+        $data = [];
+        $total = 0;
+
+        foreach($panier as $id => $quantity){
+            $plat = $platRepository->find($id);
+
+            $data[] = [
+                'plat' => $plat,
+                'quantity' => $quantity
+            ];
+            $total += $plat->getPrix() * $quantity;
+        }
+
+        return $this->render('panier/index.html.twig', compact('data', 'total'));
+    }
+
+    #[Route('/ajout/{id}', name: 'ajout')]
+    public function add(Plat $plat, SessionInterface $session)
+    {
+        // On récupère l'id du produit
+        $id = $plat->getId();
+
+        // On récupère le panier existant
+        $panier = $session->get('panier', []);
+
+        // On ajoute le produit dans le panier s'il n'y est pas encore
+        // Sinon on incrémente sa quantité
+        if(empty($panier[$id])){
+            $panier[$id] = 1;
+        }else{
+            $panier[$id]++;
+        }
+
+        $session->set('panier', $panier);
+        
+        // On redirige vers la page du panier
+        return $this->redirectToRoute('panier_index');
     }
 }
